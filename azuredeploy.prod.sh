5623b0284ecd0efebc33866b402f695fa8269a14
nickward13 <nickward@microsoft.com>
2017-11-22 19:52:46 +1100

    put vm on existing jumpbox subnet

--- azuredeploy.prod.sh
+++ azuredeploy.prod.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 az group create --name hectagon-jumpbox --location "Australia Southeast"
-az vm create --name hectagon-jumpbox --resource-group hectagon-jumpbox --image ubuntults --size Standard_DS2 --ssh-key-value @~/.ssh/id_rsa.pub --vnet-name hectagonvnet --subnet jumpbox --public-ip-address-dns-name hectagon-jumpbox
+SUBNET_ID=$(az network vnet subnet list --resource-group hectagon-shared --vnet-name hectagonvnet \
+    --query "[?name == 'jumpbox'].id" -o tsv)
+az vm create --name hectagon-jumpbox --resource-group hectagon-jumpbox --image ubuntults --size Standard_DS2 --ssh-key-value @~/.ssh/id_rsa.pub --subnet $SUBNET_ID --public-ip-address-dns-name hectagon-jumpbox
 IP_ADDRESS=$(az vm list-ip-addresses --resource-group hectagon-jumpbox --name hectagon-jumpbox \
     --query "[0].virtualMachine.network.publicIpAddresses[0].ipAddress" -o tsv)
 ssh -i ~/.ssh/id_rsa $IP_ADDRESS sudo apt-get update
