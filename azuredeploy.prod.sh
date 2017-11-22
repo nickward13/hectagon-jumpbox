99f97caff454c457b1d0d0b6c4f51970b8a7f28d
nickward13 <nickward@microsoft.com>
2017-11-22 16:25:35 +1100

    get ip address and update vnet to vnet-name

--- azuredeploy.prod.sh
+++ azuredeploy.prod.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 az group create --name hectagon-jumpbox --location "Australia Southeast"
-az vm create --name hectagon-jumpbox --resource-group hectagon-jumpbox --image ubuntults --size Standard_DS2 --ssh-key-value @~/.ssh/id_rsa.pub --vnet hectagonvnet --subnet jumpbox --public-ip-address-dns-name hectagon-jumpbox
+az vm create --name hectagon-jumpbox --resource-group hectagon-jumpbox --image ubuntults --size Standard_DS2 --ssh-key-value @~/.ssh/id_rsa.pub --vnet-name hectagonvnet --subnet jumpbox --public-ip-address-dns-name hectagon-jumpbox
+IP_ADDRESS=$(az vm list-ip-addresses --resource-group hectagon-jumpbox --name hectagon-jumpbox \
+    --query "[0].virtualMachine.network.publicIpAddresses[0].ipAddress" -o tsv)
 
