85788b3f3682bd03fa482815f1162a4b945da986
nickward13 <nickward@microsoft.com>
2017-12-18 13:12:16 +1100

    updated linux vm size

--- azuredeploy.prod.sh
+++ azuredeploy.prod.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 az group create --name hectagon-jumpbox --location "West US" --tags Permanent=True
 SUBNET_ID=$(az network vnet subnet list --resource-group hectagon-shared --vnet-name hectagonvnet --query "[?name == 'jumpbox'].id" -o tsv)
-az vm create --name hectagonlinux --resource-group hectagon-jumpbox --size Standard_DS2 --subnet $SUBNET_ID --image ubuntults --ssh-key-value @~/.ssh/id_rsa.pub 
+az vm create --name hectagonlinux --resource-group hectagon-jumpbox --size Standard_D2s_V3 --subnet $SUBNET_ID --image ubuntults --ssh-key-value @~/.ssh/id_rsa.pub 
 az vm create --name hectagonwin --resource-group hectagon-jumpbox --size Standard_D2s_v3 --subnet $SUBNET_ID --image Win2016Datacenter --admin-username nickward
 az vm create --name hectagondevvm --resource-group hectagon-jumpbox --size Standard_D2s_v3 --subnet $SUBNET_ID --image VS-2017-Ent-Latest-Win10-N --admin-username nickward
 LINUX_IP_ADDRESS=$(az vm list-ip-addresses --resource-group hectagon-jumpbox --name hectagonlinux --query "[0].virtualMachine.network.publicIpAddresses[0].ipAddress" -o tsv)
