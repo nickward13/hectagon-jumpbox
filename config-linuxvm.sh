a61ccab921d7929c48c45c40c6e1f0fe7d3d52a4
nickward13 <nickward@microsoft.com>
2017-12-18 13:26:57 +1100

    added minikube to config

--- config-linuxvm.sh
+++ config-linuxvm.sh
@@ -3,4 +3,5 @@ LINUX_IP_ADDRESS=$(az vm list-ip-addresses --resource-group hectagon-jumpbox --n
 ssh -i ~/.ssh/id_rsa $LINUX_IP_ADDRESS sudo apt-get update
 ssh -i ~/.ssh/id_rsa $LINUX_IP_ADDRESS sudo apt install docker.io curl nodejs npm virtualbox
 ssh -i ~/.ssh/id_rsa $LINUX_IP_ADDRESS sudo apt-get upgrade
+ssh -i ~/.ssh/id_rsa $LINUX_IP_ADDRESS curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.20.0/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
 echo "IP Address is $LINUX_IP_ADDRESS"
